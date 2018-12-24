0da855119c13684e7b9b31aa4a345918dabe71b7
Seraphine Young <seraphine_ene@yahoo.com>
2018-12-24 19:44:42 +0000

    Adding if and else to the code

--- blockchain.py
+++ blockchain.py
@@ -7,32 +7,48 @@ def get_last_blockchain_value():
 def add_value(transaction_amount,last_transaction=[1]):
     blockchain.append([last_transaction,transaction_amount])
 
-def get_user_input():
+def get_transaction_value():
     return float(input('Transaction Amount please ?:'))
-    
-tx_amount = get_user_input()
+
+def get_user_choice():
+    user_input = input('Your choice ')
+    return user_input
+
+def print_blockchain_elements():
+    #output blockchain to the console
+    for block in blockchain:
+        print(block)
+
+   
+tx_amount = get_transaction_value()
 #saving the value of input function in a variable calle tx_amount
 add_value(tx_amount)
 
-# while True:
-#     tx_amount = get_user_input()
-#     add_value(transaction_amount=tx_amount,last_transaction=get_last_blockchain_value())
+while True:
+    print('please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    user_choice = get_user_choice()
+    if user_choice == 1:
+        tx_amount = get_transaction_value()
+        add_value(transaction_amount=tx_amount,last_transaction=get_last_blockchain_value())
 
-#     for block in blockchain:
-#         print(block)
+    else:
+        print_blockchain_elements()
 
+print(done)   
 
-add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount)
+# add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount)
 
 #seraphine if you notice the order of parameter is switched when calling the function. This is because I used a keyword arguments. It does not matter the order as long as you the paraments of the functions to the arguments when calling it.
 
-tx_amount = get_user_input()
-add_value(transaction_amount=tx_amount,last_transaction=get_last_blockchain_value())
+# tx_amount = get_user_input()
+# add_value(transaction_amount=tx_amount,last_transaction=get_last_blockchain_value())
 
-tx_amount = get_user_input()
-add_value(last_transaction =get_last_blockchain_value(),transaction_amount=tx_amount)
+# tx_amount = get_user_input()
+# add_value(last_transaction =get_last_blockchain_value(),transaction_amount=tx_amount)
 
-print(blockchain)
+# print(blockchain)
 
 
 
