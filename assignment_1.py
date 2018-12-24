bc934340e9aebb9990b5e0e70c49c25a0921b8e8
Seraphine Young <seraphine_ene@yahoo.com>
2018-12-24 18:29:24 +0000

    working through the first homework on udemy

---
+++ assignment_1.py
@@ -0,0 +1,15 @@
+name = 'seraphine'
+age = 29
+
+def data_string():
+    print(str(name + str(age)))
+
+def anydata_string(name,age):
+    print(str(name + str(age)))
+
+def num_of_decade():
+    print(age / 10) 
+
+num_of_decade()
+data_string()
+anydata_string('etubom',30)
