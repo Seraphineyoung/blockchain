82d264b663dfc531285e7690eeedeedae606c518
Seraphine Young <seraphine_ene@yahoo.com>
2019-01-03 12:53:31 +0000

    refactoring initial function

--- blockchain.py
+++ blockchain.py
@@ -8,12 +8,13 @@ def add_value(transaction_amount,last_transaction=[1]):
     blockchain.append([last_transaction,transaction_amount])
 
 def get_transaction_value():
-    return float(input('Transaction Amount please ?:'))
+    return (input('Transaction Amount please ?:'))
+
 
 def get_user_choice():
-    user_input = input('Your choice ')
-    return user_input
-
+    user_input = input('Your choice? ')
+    return user_input 
+    
 def print_blockchain_elements():
     #output blockchain to the console
     for block in blockchain:
@@ -28,27 +29,27 @@ while True:
     print('please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('q: Quit')
+
     user_choice = get_user_choice()
-    if user_choice == 1:
+    if user_choice == '1':
         tx_amount = get_transaction_value()
         add_value(transaction_amount=tx_amount,last_transaction=get_last_blockchain_value())
 
-    else:
+    elif user_choice == '2':
         print_blockchain_elements()
 
-print(done)   
+    elif user_choice == 'q':
+         break
+    else:
+        print('Invalid input')
 
-# add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount)
+print('done')   
+
+
 
 #seraphine if you notice the order of parameter is switched when calling the function. This is because I used a keyword arguments. It does not matter the order as long as you the paraments of the functions to the arguments when calling it.
 
-# tx_amount = get_user_input()
-# add_value(transaction_amount=tx_amount,last_transaction=get_last_blockchain_value())
-
-# tx_amount = get_user_input()
-# add_value(last_transaction =get_last_blockchain_value(),transaction_amount=tx_amount)
-
-# print(blockchain)
 
 
 
